aeda145d8f72cc9c9634afa6c4a5d9bc1af77ef3
Orzhiang <chuan-_-liang@live.com>
2015-02-08 23:52:49 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -26,5 +26,4 @@ png(
 hist(Elect.Data.Sub$Global_active_power,col="red",main="Global Active Power",
      xlab="Global Active Power (kilowatts)",
      ylab="Frequency")
-#dev.copy(png, file = "plot1.png")
 dev.off()
